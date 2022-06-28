13515f86b4189107fd2d949347d07ef4de2a047f
ljh0608 <m98m@naver.com>
2022-06-29 03:11:18 +0900

    callbackFunction

--- callbackFuction.js
+++ callbackFuction.js
@@ -18,20 +18,46 @@
 
 // repeat(5,logOdds); //1 3
 
-var res = [1,2,3].map(function(item) {
-    return item*2;
-});
+// var res = [1,2,3].map(function(item) {
+//     return item*2;
+// });
 
-console.log(res); //[2,4,6]
+// console.log(res); //[2,4,6]
 
-res=[1,2,3].filter(function(item){
-    return item%2;
-});
+// res=[1,2,3].filter(function(item){
+//     return item%2;
+// });
 
-console.log(res); //[1,3]
+// console.log(res); //[1,3]
 
-res=[1,2,3].reduce(function(acc,cur) {
-    return acc+cur;
-},0);
+// res=[1,2,3].reduce(function(acc,cur) {
+//     return acc+cur;
+// },0);
 
-console.log(res); // 6
+// console.log(res); // 6
+
+
+
+
+
+function checkmood(mood,goodCallback,badCallback){
+    if(mood==="good"){
+        goodCallback();
+    }
+
+    else{
+        badCallback();
+    }
+}
+
+function cry(){
+    console.log('ACTION :: CRY');
+}
+
+function sing(){
+    console.log('ACTION :: SING');
+}
+
+checkmood('sad',sing,cry);
+
+
