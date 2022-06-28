7d55728782448e32efe0bd5b315d0fdc427e877e
ljh0608 <m98m@naver.com>
2022-06-28 10:03:05 +0900

    19장 prototype

--- prototype.js
+++ prototype.js
@@ -53,18 +53,103 @@
 // console.log(({}).hasOwnProperty('prototype')); // false
 
 
-// 함수 정의가 평가되어 함수 객체를 생성하는 시점에 프로토타입도 생성됨
-console.log(Person.prototype); //{}
+// // 함수 정의가 평가되어 함수 객체를 생성하는 시점에 프로토타입도 생성됨
+// console.log(Person.prototype); //{}
 
-// 생성자 함수
-function Person(name){
-    this.name=name;
+// // 생성자 함수
+// function Person(name){
+//     this.name=name;
+// }
+
+// // 화살표 함수는 non-constructor이다.
+// const Person2=name=>{
+//     this.name=name;
+// };
+
+// //non-constructor는 프로토타입이 생성되지 않는다.
+// console.log(Person2.prototype); //undefined
+
+
+
+
+// // 생성자 함수
+// function Person(name){
+//     this.name=name;
+// }
+
+// const me = new Person('LEE');
+
+// function isInstanceof(instance, constructor){
+//     // 프로토타입 취득
+//     const prototype= Object.getPrototypeOf(instance);
+
+//     // 재귀 탈출 조건
+//     if(prototype===null)
+//         return false;
+
+//     return prototype===constructor.prototype || isInstanceof(prototype,constructor);
+// }
+
+// console.log(isInstanceof(me,Person));
+// console.log(isInstanceof(me,Object));
+
+
+// let obj=Object.create(null);
+// console.log(Object.getPrototypeOf(obj)===null); //true
+
+// obj=Object.create(Object.prototype);
+// console.log(Object.getPrototypeOf(obj)===Object.prototype); //true
+
+
+// const myproto={x:10};
+
+// const obj={
+//     y:10,
+//     __proto__:myproto
+// };
+
+// console.log(Object.getPrototypeOf(obj)===myproto);// true
+
+// //정적 프로퍼티 메서드
+// function Person(name){
+//     this.name=name;
+// }
+
+// Person.prototype.sayHello=()=>{
+//     console.log('hi');
+// };
+
+// Person.staticMethod=()=>{
+//     console.log('staticMethod');
+// };
+
+// const me=new Person('LEE');
+// Person.staticMethod(); //staticMethod
+// me.staticMethod(); //TypeError: me.staticMethod is not a function
+
+
+//for .. in문
+const person={
+    name: 'LEE',
+    address:'Ilsan',
+    __proto__:{age:20}
 }
 
+for(const i in person){
+    console.log(person[i]);
+ }
+// LEE
+// Ilsan
+// 20
 
-const Person2=name=>{
-    this.name=name;
-};
+console.log(Object.keys(person)); //[ 'name', 'address' ]
+console.log(Object.values(person)); // [ 'LEE', 'Ilsan' ]
+console.log(Object.entries(person));
 
-console.log(Person2.prototype);
+
+
+
+
+
+
 
