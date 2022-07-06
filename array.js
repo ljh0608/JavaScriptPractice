e69d9a0fcec536a9cb324b570ef8159294d0b11c
ljh0608 <m98m@naver.com>
2022-07-06 16:32:26 +0900

    27장 array

--- array.js
+++ array.js
@@ -1,7 +1,7 @@
 // const arr=[1,2,3];
 
 const { Type } = require("@angular/core");
-const { indexOf } = require("lodash");
+const { indexOf, set } = require("lodash");
 
 // console.log(arr.length); //3
 
@@ -340,30 +340,253 @@ const { indexOf } = require("lodash");
 // console.log(point); //[ 200, 100, 40, 25, 10, 5 ]
 
 
-const todo = [
-    {id:2, content: 'HTML'},
-    {id:6 , content:'CSS'},
-    {id:3, content:'JAVASCIPT'}
-];
+// const todo = [
+//     {id:2, content: 'HTML'},
+//     {id:6 , content:'CSS'},
+//     {id:3, content:'JAVASCIPT'}
+// ];
 
-function compare(key){
-    // 문자열인 경우 - 산술 연산으로 비교시 NaN이 나오므로 비교연산 사용
-    return (a,b)=>(a[key]>b[key]?1 : (a[key]<b[key]?-1:0));
-}
+// function compare(key){
+//     // 문자열인 경우 - 산술 연산으로 비교시 NaN이 나오므로 비교연산 사용
+//     return (a,b)=>(a[key]>b[key]?1 : (a[key]<b[key]?-1:0));
+// }
 
-todo.sort(compare('id'));
+// todo.sort(compare('id'));
 
-console.log(todo);
-// [
-//     { id: 2, content: 'HTML' },
-//     { id: 3, content: 'JAVASCIPT' },
-//     { id: 6, content: 'CSS' }
-//   ]
+// console.log(todo);
+// // [
+// //     { id: 2, content: 'HTML' },
+// //     { id: 3, content: 'JAVASCIPT' },
+// //     { id: 6, content: 'CSS' }
+// //   ]
 
-todo.sort(compare('content'));
-console.log(todo);
-// [
-//     { id: 6, content: 'CSS' },
-//     { id: 2, content: 'HTML' },
-//     { id: 3, content: 'JAVASCIPT' }
-//   ]
+// todo.sort(compare('content'));
+// console.log(todo);
+// // [
+// //     { id: 6, content: 'CSS' },
+// //     { id: 2, content: 'HTML' },
+// //     { id: 3, content: 'JAVASCIPT' }
+// //   ]
+
+
+
+// const numbers =[1,2,3];
+// const pows=[];
+
+// numbers.forEach(item=> pows.push(item**2));
+// console.log(pows); //[ 1, 4, 9 ]
+
+
+// [1,2,3].forEach((item, index, arr)=>{
+//     console.log(`요소값: ${item}, 인덱스: ${index}, this: ${JSON.stringify(arr)}`);
+// });
+// // 요소값: 1, 인덱스: 0, this: [1,2,3]
+// // 요소값: 2, 인덱스: 1, this: [1,2,3]
+// // 요소값: 3, 인덱스: 2, this: [1,2,3]
+
+
+// const numbers=[1,2,3];
+// numbers.forEach((item,index,arr)=>{arr[index]=item**2});
+// console.log(numbers); //[ 1, 4, 9 ]
+
+
+// const result=[1,2,3].forEach((item)=>item*2);
+// console.log(result); //undefined
+
+
+// class Numbers{
+//     numberArray=[];
+
+//     multiply(arr){
+//         arr.forEach(function(item){
+//             this.numberArray.push(item*item);
+//         },this); //forEach 메서드의 콜백 함수 내부에서 this로 사용할 객체를 전달
+//     }
+// }
+
+// const numbers=new Numbers();
+// numbers.multiply([1,2,3]);
+// console.log(numbers.numberArray); //[1,4,9]
+
+// class Numbers{
+//     numberArray=[];
+
+//     multiply(arr){
+//         arr.forEach((item)=>{ //화살표 함수 사용
+//             this.numberArray.push(item*item);
+//         });
+//     }
+// }
+
+// const numbers=new Numbers();
+// numbers.multiply([1,2,3]);
+// console.log(numbers.numberArray); //[1,4,9]
+
+// const numbers=[1,4,9];
+
+// const roots=numbers.map(item=>Math.sqrt(item));
+// //위 코드는 아래와 같다.
+// //const roots=numbers.map(Math.sqrt);
+
+// //원본 배열 변경 X
+// console.log(numbers); //[ 1, 4, 9 ]
+// //새로운 배열 반환
+// console.log(roots); //[ 1, 2, 3 ]
+
+
+// class Prefixer{
+//     constructor(prefix){
+//         this.prefix=prefix;
+//     }
+
+//     add(arr){
+//         //화살표 함수 내부에서 .this를 참조하면 상위 스코프의 this를 그대로 참조한다.
+//         return arr.map(item=>this.prefix + item);
+//     }
+// }
+
+// const prefix=new Prefixer('-webkit-');
+
+// console.log(prefix.add(['transition','user-select']));
+
+
+//filter
+
+// const numbers=[1,2,3,4,5];
+// const odds=numbers.filter(item=>item%2);
+
+// console.log(odds); //[ 1, 3, 5 ]
+
+
+
+// class Users{
+//     constructor(){
+//         this.users=[
+//             {id:1, name:'lee'},
+//             {id:2, name:'kim'}
+//         ];
+//     }
+
+//     findById(id){
+//         //id가 일치하는 사용자만 반환한다.
+//         return this.users.filter(user=>user.id===id);
+//     }
+
+//     removeId(id){
+//         //id가 일치하지 않는 사용자를 제거한다.
+//         this.users=this.users.filter(user=>user.id!==id);
+//     }
+// }
+
+// const users=new Users();
+
+// console.log(users); //Users { users: [ { id: 1, name: 'lee' }, { id: 2, name: 'kim' } ] }
+
+// let user=users.findById(1);
+// console.log(user); //[ { id: 1, name: 'lee' } ]
+
+// users.removeId(1);
+
+// user=users.findById(1);
+// console.log(user); //[]
+
+
+// const values=[1,2,3,4,5,6];
+
+// const average=values.reduce((acc,cur,i,{length})=>{
+//     return i===length-1? (acc+cur)/length:acc+cur;
+// },0);
+
+// console.log(average); //3.5
+
+// const values=[5,4,2,1,3];
+
+// const max_num=values.reduce((acc,cur)=>{
+//     return acc>cur?acc:cur;
+// },values[0]);
+
+// console.log(max_num); //5
+
+// //최대값은 Math.max 메서드를 사용하는 방법이 더 직관적
+// const value=[5,4,2,1,3];
+// const max=Math.max(...value);
+// console.log(max); //5
+
+
+// const fruit =['banana','apple','orange','orange','apple'];
+
+// const count=fruit.reduce((acc,cur)=>{
+//     acc[cur]=(acc[cur]||0)+1;
+//     return acc;
+// },{});
+
+// console.log(count); //{ banana: 1, apple: 2, orange: 2 }
+
+
+// const values=[1,[2,3],4,[5,6]];
+
+// const flatten=values.reduce((acc,cur)=>acc.concat(cur),[]);
+
+// console.log(flatten); //[ 1, 2, 3, 4, 5, 6 ]
+
+
+// const values =[1,2,1,3,5,4,5,3,4,4];
+
+// const result =[...new Set(values)];
+// console.log(result); //[ 1, 2, 3, 5, 4 ]
+
+// const products =[
+//     {id:1, price: 100},
+//     {id:2, price: 200},
+//     {id:3, price: 300},
+// ];
+
+// const priceSum=products.reduce((acc,cur)=>acc+cur.price,0);
+// console.log(priceSum); //600
+
+
+// console.log([5,10,15].some((item)=>item>10)); //true
+// console.log([5,10,15].some((item)=>item>20)); //false
+
+// console.log(['apple','banana','orange'].some((item)=>item==='apple')); //true
+
+
+// console.log([5,10,15].every(item=>item>3)); //true
+// console.log([5,10,15].every(item=>item>6)); //false
+
+// console.log([].every(item=>item>3)); //true
+
+
+// const users=[{id:1, name:'lee'},
+// {id:2, name:'choi'},
+// {id:3, name:'kim'},
+// {id:4, name:'park'},
+// ];
+
+// console.log(users.find(user=>user.id===2)); //{ id: 2, name: 'choi' }
+
+
+// [1,2,2,4].filter(item=>item===2); //[2,2]
+
+// [1,2,2,4].find(item=>item===2); // 2
+
+
+// const users=[{id:1, name:'lee'},
+// {id:2, name:'choi'},
+// {id:3, name:'kim'},
+// {id:4, name:'park'},
+// ];
+
+// console.log(users.findIndex(user=>user.id===2)); //1
+// console.log(users.findIndex(user=>user.name==='kim')); //2
+// console.log(users.findIndex(user=>user.name==='Ji')); //-1
+
+
+
+
+const arr=['hello','world'];
+
+ console.log(arr.map(x=>x.split(''))); //[ [ 'h', 'e', 'l', 'l', 'o' ], [ 'w', 'o', 'r', 'l', 'd' ] ]
+console.log(arr.map(x=>x.split('')).flat()); //['h', 'e', 'l', 'l','o', 'w', 'o', 'r','l', 'd']
+
+console.log(arr.flatMap(x=>x.split('')));
