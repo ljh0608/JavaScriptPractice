be1f7841f35408c2ef5f6b6610d0848eff37f73c
ljh0608 <m98m@naver.com>
2022-07-07 21:25:50 +0900

    29장 Math

---
+++ Math.js
@@ -0,0 +1,68 @@
+
+// console.log(Math.PI); //3.141592653589793
+
+
+// console.log(Math.abs(-1)); //1
+// console.log(Math.abs('-1')); //1
+// console.log(Math.abs(1)); //1
+// console.log(Math.abs([])); //0
+// console.log(Math.abs(null)); //0
+// console.log(Math.abs(undefined)); //NaN
+// console.log(Math.abs('')); //0
+
+// console.log(Math.round(1.4)); //1
+// console.log(Math.round(1.6)); //2
+// console.log(Math.round(-1.4)); //-1
+// console.log(Math.round(-1.6)); //-2
+// console.log(Math.round(1)); //1
+// console.log(Math.round()); //NaN
+
+// console.log(Math.ceil(1.4)); //2
+// console.log(Math.ceil(1.6)); //2
+// console.log(Math.ceil(-1.4)); //-1
+// console.log(Math.ceil(-1.6)); //-1
+// console.log(Math.ceil(1)); //1
+// console.log(Math.ceil()); //NaN
+
+
+
+// console.log(Math.floor(1.4)); //1
+// console.log(Math.floor(1.6)); //1
+// console.log(Math.floor(-1.4)); //-2
+// console.log(Math.floor(-1.6)); //-2
+// console.log(Math.floor(1)); //1
+// console.log(Math.floor()); //NaN
+
+// console.log(Math.sqrt(9)); //3
+// console.log(Math.sqrt(-9)); //NaN
+// console.log(Math.sqrt(2)); //1.4142135623730951
+// console.log(Math.sqrt(1)); //1
+
+
+// console.log(Math.random()); //랜덤 실수0.49109064635614974
+
+// const random=Math.floor((Math.random()*10)+1);
+// console.log(random); //1~10 범위의 랜덤 정수
+
+// console.log(Math.pow(2,2)); //4
+// console.log(Math.pow(5,2)); //25
+
+// console.log(2**2**2); //16
+// console.log(Math.pow(Math.pow(2,2),2)); //16
+
+
+// console.log(Math.max(1,2,3)); //3
+
+// //배열의 요소중 최대값을 구하려면
+// //ES6 스프레드 문법
+// console.log(Math.max(...[1,2,3])); //3
+// console.log(Math.max([1,2,3])); //NaN
+
+
+console.log(Math.min(1,2,3)); //3
+
+//배열의 요소중 최소값을 구하려면
+//ES6 스프레드 문법
+console.log(Math.min(...[1,2,3])); //1
+console.log(Math.min([1,2,3])); //NaN
+
