eca1d27f196010c572c55b7e16425fd2fb849e89
ljh0608 <m98m@naver.com>
2022-07-10 12:32:34 +0900

    28장 Number

--- Number.js
+++ Number.js
@@ -95,7 +95,7 @@
 // console.log((12345.6789).toPrecision(6)); //12345.7
 
 
-console.log((10).toString());
-console.log((10).toString(2));
+// console.log((10).toString()); //10
+// console.log((10).toString(2)); //1010
+// console.log((10).toString(8)); //12
 
-console.log((10).toString(8));
