0efab382c54e45400f7abd5e143b399198908329
ljh0608 <m98m@naver.com>
2022-07-16 23:54:08 +0900

    37장 Set_Map

--- Set_Map.js
+++ Set_Map.js
@@ -7,5 +7,21 @@
 
 
 
-const uniq=array=>array.filter((v,i,self)=>self.indexOf(v)===i);
-console.log(uniq([2,1,2,3,4,3,4])); //[ 2, 1, 3, 4 ]
+//여기서부터
+// const uniq=array=>array.filter((v,i,self)=>self.indexOf(v)===i);
+// console.log(uniq([2,1,2,3,4,3,4])); //[ 2, 1, 3, 4 ]
+
+
+const {size} =new Set([1,2,3,3]);
+console.log(size); // 3
+
+
+const set = new Set();
+console.log(set); //Set(0) {}
+
+set.add(1);
+console.log(set); // Set(1) { 1 }
+
+set.add(1).add(2);
+console.log(set); // Set(2) { 1, 2 }
+
