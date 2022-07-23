54a75a660abcea835562e60ceac3b6ce8230dcb9
ljh0608 <m98m@naver.com>
2022-07-23 21:33:10 +0900

    Set & Map

--- Set_Map.js
+++ Set_Map.js
@@ -242,14 +242,23 @@ const map=new Map([[lee,'developer'],[kim,'designer']]);
 //Map.prototype.keys는 Map 객체에서 요소키를 값으로 갖는 이터레이터 반환
 for(const key of map.keys()){
     console.log(key);
+//  { name: 'lee' }
+// { name: 'kim' }
 }
 
+// 요소값을 갖는 이터레이터 반환
 for(const value of map.values()){
     console.log(value);
+//     developer
+//      designer
 }
 
+//요소키와 요소값을 갖는 이터레이터 반환
 for(const entry of map.entries()){
     console.log(entry);
+//     [ { name: 'lee' }, 'developer' ]
+// [ { name: 'kim' }, 'designer' ]
+
 }
 
 
