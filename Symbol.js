08e53494373861add5b19f399c151b1324edcbae
ljh0608 <m98m@naver.com>
2022-07-23 21:33:37 +0900

    Symbol

--- Symbol.js
+++ Symbol.js
@@ -74,4 +74,5 @@ Array.prototype[Symbol.for('sum')]= function(){
     return this.reduce((acc, cur) => acc+cur,0);
 };
 
-console.log([1,2][Symbol.for('sum')]());
+console.log([1,2][Symbol.for('sum')]()); //3
+
