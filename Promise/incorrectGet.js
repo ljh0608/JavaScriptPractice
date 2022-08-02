933fdfc936af498d920a96fcddb255f0b37cdb46
ljh0608 <m98m@naver.com>
2022-08-02 16:38:17 +0900

    45장 Promise

--- Promise/incorrectGet.js
+++ Promise/incorrectGet.js
@@ -1,16 +1,41 @@
 
-const get= url=>{
+// let todos;
+
+// const get= url=>{
+//     const xhr=new XMLHttpRequest();
+//     xhr.open('GET',url);
+//     xhr.send();
+
+//     xhr.onload=()=>{
+//         if(xhr.status===200){
+//             //상위 스코프 변수에 할당
+//             todos= JSON.parse(xhr.response);
+//         }
+//         console.error(`${xhr.status} ${xhr.statusText}`);
+//     };
+// };
+
+// get('https://jsonplaceholder.typicode/com/posts/1');
+// console.log(todos); //undefined
+
+const get=(url,successCallback, failureCallback)=>{
     const xhr=new XMLHttpRequest();
     xhr.open('GET',url);
     xhr.send();
 
     xhr.onload=()=>{
         if(xhr.status===200){
-            return JSON.parse(xhr.response);
+            //성공했을 때 서버의 응답을 콜백 함수 인수로 전달 호출
+            successCallback(JSON.parse(xhr.response));
+        }else{
+            //실패했을 때 에러 정보를 콜백함수 인수로 전달 호출
+            failureCallback(xhr.status);
         }
-        console.error(`${xhr.status} ${xhr.statusText}`);
     };
 };
 
-const response =get('https://jsonplaceholder.typicode/com/posts/1');
-console.log(response);
+get('https://jsonplaceholder.typicode.com/posts/1',console.log, console.error);
+ //{userId: 1, id: 1, title: "sunt aut ...}
+
+
+
