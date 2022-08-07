583b53cea2d8b22ce086d5fff477e4482dd9fd6c
ljh0608 <m98m@naver.com>
2022-08-07 18:11:00 +0900

    45장 Promise _ method

--- Promise/promise_method.js
+++ Promise/promise_method.js
@@ -1,9 +1,9 @@
 
 
 
-//promise resolve
+// //promise resolve
 
-//배열을 resolve하는 프로미스 생성
+// //배열을 resolve하는 프로미스 생성
 //     const resolvedPromise=Promise.resolve([1,2,3]);
 // resolvedPromise.then(console.log); //[ 1, 2, 3 ]
 
@@ -12,8 +12,8 @@
 // resolved.then(console.log); //[ 1, 2, 3 ]
 
 
-// //promise reject
-// //에러 객체를 reject 하는 프로미스 생성
+//promise reject
+//에러 객체를 reject 하는 프로미스 생성
 // const rejectedPromise=Promise.reject(new Error('Error!'));
 // rejectedPromise.catch(console.log); //Error: Error!
 
@@ -23,18 +23,18 @@
 
 
 //promise.all
-const requestData=()=>
-    new Promise(resolve =>setTimeout(()=>resolve(1),3000));
-const requestData2=()=>
-    new Promise(resolve =>setTimeout(()=>resolve(2),2000));
-const requestData3=()=>
-    new Promise(resolve =>setTimeout(()=>resolve(3),1000));
+// const requestData=()=>
+//     new Promise(resolve =>setTimeout(()=>resolve(1),3000));
+// const requestData2=()=>
+//     new Promise(resolve =>setTimeout(()=>resolve(2),2000));
+// const requestData3=()=>
+//     new Promise(resolve =>setTimeout(()=>resolve(3),1000));
 
 
-    Promise.all([requestData(),requestData2(),requestData3()])
-    .then(console.log) //[1,2,3] 약 3초 소요
-    .catch(console.error);
 
+//     Promise.all([requestData(),requestData2(),requestData3()])
+//     .then(console.log) //[1,2,3] 약 3초 소요
+//     .catch(console.error);
 
     // const res=[];
     // requestData()
@@ -53,11 +53,51 @@ const requestData3=()=>
     // .catch(console.error);
 
 
+// Promise.all([
+//     1, //Promise.resolve(1)
+//     2, //Promise.resolve(2)
+//     3 //Promise.resolve(3)
+// ]).then(console.log) //[ 1, 2, 3 ]
+// .catch(console.error);
 
-Promise.all([
-    1, //Promise.resolve(1)
-    2, //Promise.resolve(2)
-    3 //Promise.resolve(3)
-]).then(console.log) //[ 1, 2, 3 ]
-.catch(console.error);
+
+//Promise.race
+
+    // Promise.race([
+    //     new Promise(resolve=> setTimeout(()=> resolve(1),3000)),
+    //     new Promise(resolve=> setTimeout(()=> resolve(2),2000)),
+    //     new Promise(resolve=> setTimeout(()=> resolve(3),1000)),
+
+    // ])
+    // .then(console.log) //3 가장 빠른 처리결과
+    // .catch(console.log)
+
+    // Promise.race([
+    //     new Promise((_,reject)=> setTimeout(()=> reject(new Error('Error1')),3000)),
+    //     new Promise((resolve,reject)=> setTimeout(()=> resolve(1),1000)),
+    //     new Promise((_,reject)=> setTimeout(()=> reject(new Error('Error3')),2000)),
+
+    // ])
+    // .then(console.log) //1
+    // .catch(console.log)
+
+
+
+//Promise.allSettled
+ Promise.allSettled([
+    new Promise(resolve=> setTimeout(()=>resolve(1),2000)),
+    new Promise((_,reject)=>setTimeout(()=>reject(new Error('Error !')),1000))
+ ]).then(console.log);
+
+//  [
+//     { status: 'fulfilled', value: 1 },
+//     {
+//       status: 'rejected',
+//       reason: Error: Error !
+//           at Timeout._onTimeout (c:\Users\m9797\project folder\Js_practice\Promise\promise_method.js:89:51)
+//           at listOnTimeout (internal/timers.js:554:17)
+//           at processTimers (internal/timers.js:497:7)
+//     }
+//   ]
+
 
