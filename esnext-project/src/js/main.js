94e07c6df279c5070a945b89b08cd0ffa9634f40
ljh0608 <m98m@naver.com>
2022-11-25 12:51:09 +0900

    Udemy_JS

--- esnext-project/src/js/main.js
+++ esnext-project/src/js/main.js
@@ -1,4 +1,5 @@
-
+//src/js/main.js
+import "@babel/polyfill";
 import {pi,power, Foo} from "./lib";
 
 console.log(pi);
@@ -18,3 +19,6 @@ console.log(Object.assign({},{x:1},{y:2}));
 
 //polyfill이 필요한 코드
 console.log(Array.from([1,2,3],v=>v+v));
+
+
+
